d10e88c12870b7d3ba8c895caec4e31631bb224d
Shant <shant.asf@gmail.com>
2022-04-08 01:44:19 +0400

    add craco for custom webpack config

---
+++ craco.config.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+
+const config = {
+  webpack: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@styles': path.resolve(__dirname, 'src/assets/styles'),
+
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@common': path.resolve(__dirname, 'src/components/common'),
+      '@images': path.resolve(__dirname, 'src/components/images'),
+      '@layouts': path.resolve(__dirname, 'src/components/layouts'),
+      '@shared': path.resolve(__dirname, 'src/components/shared'),
+
+      '@pages': path.resolve(__dirname, 'src/pages'),
+
+      '@configs': path.resolve(__dirname, 'src/configs'),
+
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
+
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+
+      '@store': path.resolve(__dirname, 'src/store'),
+
+      '@theme': path.resolve(__dirname, 'src/theme'),
+
+      '@appTypes': path.resolve(__dirname, 'src/types'),
+
+      '@utils': path.resolve(__dirname, 'src/utils'),
+
+      '@validations': path.resolve(__dirname, 'src/validations'),
+    },
+  },
+};
+
+export default config;
