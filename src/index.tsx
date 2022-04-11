9494d57f9233d092f1c38723fd560db60eb9d135
Shant <shant.asf@gmail.com>
2022-04-11 23:44:09 +0400

    add web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import { store } from './store';
 import { history } from './configs/history';
 import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 import { GlobalStyles } from 'assets/styles/globalStyles';
 import App from './App';
 
@@ -38,4 +39,13 @@ root.render(
   </React.StrictMode>,
 );
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
+serviceWorker.register({
+  onSuccess: () => console.log('Service Worker Registered'),
+  onUpdate: () => console.log('Service Worker Updated'),
+});
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
