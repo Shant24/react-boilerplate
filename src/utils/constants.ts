488210d41e5b2c4b227ff6172d6da1fffee8232a
Shant <shant.asf@gmail.com>
2022-05-01 00:21:43 +0400

    configure axios

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+
 export const ENVIRONMENT = {
   isDev: process.env.NODE_ENV !== 'production',
   isProd: process.env.NODE_ENV === 'production',
